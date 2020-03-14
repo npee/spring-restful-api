8a3586d9f393f676dc6d2265cff3f9bb7f5237c7
ysiosreserva <jmc4664@gmail.com>
2020-03-14 22:15:48 +0900

    init project

---
+++ src/main/java/restful/controller/HomeController.java
@@ -0,0 +1,16 @@
+package restful.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+	
+	@RequestMapping("/")
+	//@ResponseBody
+	public String hello() {
+		return "index";
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>index.jsp</title>
+	</head>
+	<body>
+		<h1>Return to index</h1>
+		<h2>by ViewResolver</h2>
+	</body>
+</html>
